e98ea3c706561f6fccb86710c7828d3022d059fe
Alexander Pugh <alexanderpugh1991@gmail.com>
2017-11-03 14:31:00 +0000

    Add salary calculation util and tests

--- __test__/utilities/calculateSalary.spec.js
+++ __test__/utilities/calculateSalary.spec.js
@@ -3,19 +3,169 @@ const calculateSalary = require('../../server/utilities/calculateSalary');
 describe('calculateSalary', () => {
   test('Should throw an error when incomeYearly in not a number', () => {
     expect(() => {
-      calculateSalary({ incomeYearly: '$5005.23'} );
+      calculateSalary({ incomeYearly: '$5005.23' });
     }).toThrow();
   });
 
-  test('Should throw an error when studentLoan type in not valid', () => {
+  test('Should throw an error when studentLoanPlan type in not valid', () => {
     expect(() => {
-      calculateSalary({ incomeYearly: 50005.23, studentLoan: 'DERRR'} );
+      calculateSalary({ incomeYearly: 50005.23, studentLoanPlan: 'DERRR' });
     }).toThrow();
   });
 
-  test('Should throw an error when age in not valid', () => {
-    expect(() => {
-      calculateSalary({ incomeYearly: 50005.23, studentLoan: 'PLAN 1', age: 55} );
-    }).toThrow();
+  test('Should return the correct takeHomeYearly value', () => {
+    const result = calculateSalary({ incomeYearly: 160000, studentLoanPlan: 'PLAN_2' });
+    expect(result.takeHomeYearly).toBe(82969.68);
+  });
+});
+
+describe('calculateSalary income tax', () => {
+  test('Should return the correct income tax for a salary less than the personal allowance', () => {
+    const result = calculateSalary({ incomeYearly: 10000, studentLoanPlan: 'PLAN_2' });
+    expect(result.incomeTaxYearly).toBe(0);
+  });
+
+  test('Should return the correct income tax for a basic rate salary', () => {
+    const result = calculateSalary({ incomeYearly: 25000, studentLoanPlan: 'PLAN_2' });
+    expect(result.incomeTaxYearly).toBe(2700);
+  });
+
+  test('Should return the correct income tax for a higer rate salary', () => {
+    const result = calculateSalary({ incomeYearly: 60000, studentLoanPlan: 'PLAN_2' });
+    expect(result.incomeTaxYearly).toBe(12700);
+  });
+
+  test('Should return the correct income tax for a higer rate salary with reduced personal allowance', () => {
+    const result = calculateSalary({ incomeYearly: 110000, studentLoanPlan: 'PLAN_2' });
+    expect(result.incomeTaxYearly).toBe(34700);
+  });
+
+  test('Should return the correct income tax for additional rate salary', () => {
+    const result = calculateSalary({ incomeYearly: 160000, studentLoanPlan: 'PLAN_2' });
+    expect(result.incomeTaxYearly).toBe(57800);
+  });
+
+  test('Should return the correct income tax for a salary that is equal to the personal allowance', () => {
+    const result = calculateSalary({ incomeYearly: 11500, studentLoanPlan: 'PLAN_2' });
+    expect(result.incomeTaxYearly).toBe(0);
+  });
+
+  test('Should return the correct income tax for a salary that is slightly above the personal allowance', () => {
+    const result = calculateSalary({ incomeYearly: 11503, studentLoanPlan: 'PLAN_2' });
+    expect(result.incomeTaxYearly).toBe(0.6);
+  });
+
+  test('Should return the correct income tax for a salary that is equal to the basic range end', () => {
+    const result = calculateSalary({ incomeYearly: 45000, studentLoanPlan: 'PLAN_2' });
+    expect(result.incomeTaxYearly).toBe(6700);
+  });
+
+  test('Should return the correct income tax for a salary that is slightly above the basic range end', () => {
+    const result = calculateSalary({ incomeYearly: 45003, studentLoanPlan: 'PLAN_2' });
+    expect(result.incomeTaxYearly).toBe(6701.20);
+  });
+});
+
+describe('calculateSalary National insurance', () => {
+  test('Should return the correct NI for a salary less than the allowance each week', () => {
+    const result = calculateSalary({ incomeYearly: 7800, studentLoanPlan: 'PLAN_2' });
+    expect(result.nationalInsuranceYearly).toBe(0);
+  });
+
+  test('Should return the correct NI for a salary that doesnt pay the additional rate', () => {
+    const result = calculateSalary({ incomeYearly: 12000, studentLoanPlan: 'PLAN_2' });
+    expect(result.nationalInsuranceYearly).toBe(460.32);
+  });
+
+  test('Should return the correct NI for a salary that does pay the additional rate', () => {
+    const result = calculateSalary({ incomeYearly: 50000, studentLoanPlan: 'PLAN_2' });
+    expect(result.nationalInsuranceYearly).toBe(4520.32);
+  });
+
+  test('Should return the correct NI for a salary that is really high', () => {
+    const result = calculateSalary({ incomeYearly: 98765432.12, studentLoanPlan: 'PLAN_2' });
+    expect(result.nationalInsuranceYearly).toBe(1978828.96);
+  });
+
+  test('Should return the correct NI for a salary that is equal to the allowance', () => {
+    const result = calculateSalary({ incomeYearly: 8164.0, studentLoanPlan: 'PLAN_2' });
+    expect(result.nationalInsuranceYearly).toBe(0);
+  });
+
+  test('Should return the correct NI for a salary that slightly above the allowance', () => {
+    const result = calculateSalary({ incomeYearly: 8167.0, studentLoanPlan: 'PLAN_2' });
+    expect(result.nationalInsuranceYearly).toBe(0.36);
+  });
+
+  test('Should return the correct NI for a salary that is equal to the upper range', () => {
+    const result = calculateSalary({ incomeYearly: 44980, studentLoanPlan: 'PLAN_2' });
+    expect(result.nationalInsuranceYearly).toBe(4417.92);
+  });
+
+  test('Should return the correct NI for a salary that slightly above the upper range (annually)', () => {
+    const result = calculateSalary({ incomeYearly: 44983, studentLoanPlan: 'PLAN_2' });
+    expect(result.nationalInsuranceYearly).toBe(4418.28);
+  });
+
+  test('Should return the correct NI for a salary that slightly above the upper range (weekly)', () => {
+    const result = calculateSalary({ incomeYearly: 45136, studentLoanPlan: 'PLAN_2' });
+    expect(result.nationalInsuranceYearly).toBe(4423.04);
+  });
+});
+
+describe('calculateSalary Student Loan', () => {
+  test('Should return the correct student loan for a salary less than the the payment amount for plan 1', () => {
+    const result = calculateSalary({ incomeYearly: 16000, studentLoanPlan: 'PLAN_1' });
+    expect(result.studentLoanYearly).toBe(0);
+  });
+
+  test('Should return the correct student loan for a salary less than the the payment amount for plan 2', () => {
+    const result = calculateSalary({ incomeYearly: 20000, studentLoanPlan: 'PLAN_2' });
+    expect(result.studentLoanYearly).toBe(0);
+  });
+
+  test('Should return the correct student loan for a salary more than the the payment amount for plan 1', () => {
+    const result = calculateSalary({ incomeYearly: 18000, studentLoanPlan: 'PLAN_1' });
+    expect(result.studentLoanYearly).toBe(20.25);
+  });
+
+  test('Should return the correct student loan for a salary more than the the payment amount for plan 2', () => {
+    const result = calculateSalary({ incomeYearly: 25000, studentLoanPlan: 'PLAN_2' });
+    expect(result.studentLoanYearly).toBe(360);
+  });
+
+   test('Should return the 0 student loan if plan is N/A', () => {
+    const result = calculateSalary({ incomeYearly: 25000, studentLoanPlan: 'N/A' });
+    expect(result.studentLoanYearly).toBe(0);
+  });
+
+  test('Should return the correct student loan for a 1000000 salary for plan 1', () => {
+    const result = calculateSalary({ incomeYearly: 1000000, studentLoanPlan: 'PLAN_1' });
+    expect(result.studentLoanYearly).toBe(88400.25);
+  });
+
+  test('Should return the correct student loan for a 1000000 salary for plan 2', () => {
+    const result = calculateSalary({ incomeYearly: 1000000, studentLoanPlan: 'PLAN_2' });
+    expect(result.studentLoanYearly).toBe(88110);
+  });
+
+  test('Should return the correct student loan for a salary that is equal to the plan 1 threshold', () => {
+    const result = calculateSalary({ incomeYearly: 17775, studentLoanPlan: 'PLAN_1' });
+    expect(result.studentLoanYearly).toBe(0);
+  });
+
+  test('Should return the correct student loan for a salary that is equal to the plan 2 threshold', () => {
+    const result = calculateSalary({ incomeYearly: 21000, studentLoanPlan: 'PLAN_2' });
+    expect(result.studentLoanYearly).toBe(0);
+  });
+
+  test('Should return the correct student loan for a salary that is slightly above the plan 1 threshold', () => {
+    const result = calculateSalary({ incomeYearly: 17778, studentLoanPlan: 'PLAN_1' });
+    expect(result.studentLoanYearly).toBe(0.27);
+  });
+
+  test('Should return the correct student loan for a salary that is slightly above the plan 2 threshold', () => {
+    const result = calculateSalary({ incomeYearly: 21003, studentLoanPlan: 'PLAN_2' });
+    expect(result.studentLoanYearly).toBe(0.27);
   });
 });
--- server/utilities/calculateSalary.js
+++ server/utilities/calculateSalary.js
@@ -1,27 +1,155 @@
 const _ = require('lodash');
 
-const studentLoanValid = studentLoan =>
-  studentLoan === 'N/A' || studentLoan === 'PLAN 1' || studentLoan === 'PLAN 2';
+const decimal = require('./decimal');
 
-const calculateSalary = ({ incomeYearly, studenLoadPlan = 'N/A', age = '<69' }) => {
+/**
+ * Check if a student plan is a valid type
+ *
+ * @param {string} plan
+ * @return {boolean}
+ */
+const studentLoanPlanValid = plan =>
+  _.isString(plan) && (plan === 'N/A' || plan === 'PLAN_1' || plan === 'PLAN_2');
+
+/**
+ * Calculate the amount of student loan paid yearly given the annual salary and the repayment plan
+ *
+ * @param {number} income
+ * @param {string} plan
+ * @return {number}
+ */
+const calculateStudentLoan = (income, plan) => {
+  const plan1Threshold = 17775;
+  const plan2Threshold = 21000;
+
+  switch (plan) {
+    case 'N/A':
+      return 0;
+    case 'PLAN_1':
+      return decimal((income <= plan1Threshold) ? 0 : (income - plan1Threshold) * 0.09);
+    case 'PLAN_2':
+      return decimal((income <= plan2Threshold) ? 0 : (income - plan2Threshold) * 0.09);
+  }
+};
+
+/**
+ * Calculate the national insurance paid yearly given the annual income
+ *
+ * @param {number} income
+ * @return {number}
+ */
+const calculateNI = income => {
+  const weekAllowance = 157;
+  const mediumRange = 865;
+
+  const basicRate = 0.12;
+  const additionalRate = 0.02;
+
+  const weeklyIncome = income / 52;
+  let ni = 0;
+
+  if (weeklyIncome > weekAllowance) {
+    if (Math.floor(weeklyIncome) > mediumRange) {
+      ni += ((mediumRange - weekAllowance) * basicRate) * 52;
+      ni += ((weeklyIncome - mediumRange) * additionalRate) * 52;
+      ni += 2;
+    } else {
+      ni = ((weeklyIncome - weekAllowance) * basicRate) * 52;
+    }
+  }
+
+  return decimal(ni);
+}
+
+/**
+ * Calculate the income tax paid yearly given the annual income
+ *
+ * @param {number} income
+ * @return {number}
+ */
+const calculateIncomeTax = (income) => {
+  const basicRate = 0.2;
+  const higherRate = 0.4;
+  const additionalRate = 0.45;
+
+  const basicRangeEnd = 33500;
+  const higherRangeEnd = 150000;
+
+  let tax = 0;
+
+  if (income > basicRangeEnd) {
+    tax += basicRangeEnd * basicRate;
+
+    if (income > higherRangeEnd) {
+      tax += (higherRangeEnd - basicRangeEnd) * higherRate;
+      tax += (income - higherRangeEnd) * additionalRate;
+    } else {
+      tax += (income - basicRangeEnd) * higherRate;
+    }
+
+  } else {
+    tax += income * basicRate;
+  }
+
+  return decimal(tax);
+};
+
+/**
+ * Calculate the amount that is income-taxable given the annual income
+ *
+ * @param {number} income
+ * @return {number}
+ */
+const calculateTaxableIncome = income => {
+  const personalAllowance = 11500;
+  const upperLimit = 100000;
+  const cutOff = 123000;
+
+  if (income < personalAllowance) {
+    return 0;
+  }
+
+  if (income > upperLimit) {
+    if (income > cutOff) {
+      return income;
+    }
+
+    const newPA = personalAllowance - (income - upperLimit) / 2;
+
+    return income - newPA;
+  }
+
+  return income - personalAllowance;
+};
+
+/**
+ * Calculate the actual salary after deductions
+ *
+ * @param {object}
+ * @param {object.number} incomeYearly - the income before reductions
+ * @param {object.string} studentLoanPlan - the student loan plan someone is on - N/A, PLAN_1, PLAN_2
+ */
+const calculateSalary = ({ incomeYearly, studentLoanPlan }) => {
   if (!_.isNumber(incomeYearly)) {
     throw new Error('ERROR: incomeYearly must be a valid number');
   }
 
-  if (!studentLoanValid(studentLoan)) {
+  if (!studentLoanPlanValid(studentLoanPlan)) {
     throw new Error(`ERROR: student loan must be either 'N/A', 'PLAN 1' or 'PLAN 2'`);
   }
 
-  if (!(age === '<69' || age === '69-78' || age === '79+')) {
-    throw new Error(`ERROR: age must be either '<69', '69-78' or '79+'`);
-  }
+  const taxableYearly = calculateTaxableIncome(incomeYearly);
+  const incomeTaxYearly = calculateIncomeTax(taxableYearly);
+  const nationalInsuranceYearly = calculateNI(incomeYearly);
+  const studentLoanYearly = calculateStudentLoan(incomeYearly, studentLoanPlan);
+  const takeHomeYearly = incomeYearly - incomeTaxYearly - nationalInsuranceYearly - studentLoanYearly;
 
   return {
-    result: {
-      totalYearly: 0.00,
-      totalMonthly: 0.00,
-      totalWeekly: 0.00
-    }
+    taxableYearly,
+    incomeTaxYearly,
+    nationalInsuranceYearly,
+    studentLoanYearly,
+    takeHomeYearly
   }
 };
 
